f12100bd240d8cf2aa75cdf13163c2a087ce539c
timlzh <2921349622@qq.com>
2024-04-19 01:08:49 +0800

    feat: add footer

--- ui/src/Footer.tsx
+++ ui/src/Footer.tsx
@@ -1,6 +1,16 @@
+import styles from './Footer.module.scss';
+import { className } from './utils.tsx';
+import nes from './nes.tsx';
+
 const Footer = () => {
     return (
-        <></>
+        <footer className={className(styles.footer)}>
+            <div className={className(styles.footerContent)}>
+                <span className={className(nes.text, nes.disabled)}>
+                    Copyright  2024-now @CNSS All Rights Reserved.
+                </span>
+            </div>
+        </footer>
     );
 };
 
